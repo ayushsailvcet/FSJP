ecc7146511c4839518a9fa9230cc9bb50d587b2f
Ayush Sail <ayush.2507097105@vcet.edu.in>
2026-07-25 11:33:54 +0530

    Update exp1.java

--- exp1.java
+++ exp1.java
@@ -1,36 +1,32 @@
 import java.util.*;
 
-class rectangle 
-{
-int l;
-int b;
+class rectangle {
+	int l;
+	int b;
 
-void accept()
-{
-Scanner t=new Scanner(System.in);
-System.out.println("Enter the lenght and breadth of a rectangle :");
-l=t.nextInt();
-b=t.nextInt();
-}
+	void accept() {
+		Scanner t=new Scanner(System.in);
+		System.out.println("Enter the lenght and breadth of a rectangle :");
+		l=t.nextInt();
+		b=t.nextInt();
+	}
 	
-void area()
-{
-int area =l*b;
-System.out.println("Area of rectangle : "+area);
-}
+	void area() {
+		int area =l*b;
+		System.out.println("Area of rectangle : "+area);
+	}
+
 }
 
 
-class rect
-{
-public static void main(String x[])
-{
-rectangle r1=new rectangle();
-r1.accept();
-r1.area();
-
-rectangle r2=new rectangle();
-r2.accept();
-r2.area();
-}
+class rect {
+	public static void main(String x[]) {
+		rectangle r1=new rectangle();
+		r1.accept();
+		r1.area();
+		
+		rectangle r2=new rectangle();
+		r2.accept();
+		r2.area();
+	}
 }
